568d6a65e3178f3b90345a1d241428a3441e2e3a
AYAM-BLESS <blessbissong@gmail.com>
2022-01-29 22:02:24 -0800

    printing datatypes of different numbers on the screen

---
+++ primitive_datatypes/numbers/num.js
@@ -0,0 +1,14 @@
+// done by ayamba bless on 29/01/2022
+let num1 = 60;
+let num2 = 111.234;
+let num3 = 7;
+let num4 = -34.456;
+let num5 = 11.45;
+let num6 = -50;
+// printing the output for each variables
+console.log(typeof(num1));
+console.log(typeof(num2));
+console.log(typeof(num3));
+console.log(typeof(num4));
+console.log(typeof(num5));
+console.log(typeof(num6));
