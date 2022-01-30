59d097c6583d4d617bf06b95c6dca63ea4fe384d
AYAM-BLESS <blessbissong@gmail.com>
2022-01-29 22:21:38 -0800

    printing the data type of not a number

---
+++ primitive_datatypes/numbers/NaN.js
@@ -0,0 +1,14 @@
+
+//done by ayamba bless 
+
+
+//it is a concept for representing values of mathematical operations which are not numbers
+
+// it is considered a number in javascript
+
+let result = 0 / 0;
+let num = 1 + NaN;
+console.log (result);
+console.log (typeof(result));
+console.log (num);
+console.log (typeof(num));
