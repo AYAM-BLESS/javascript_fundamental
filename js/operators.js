435c6817e463b5c80f9b9792148458434251680c
AYAM-BLESS <blessbissong@gmail.com>
2022-02-23 02:41:12 -0800

    some operators in javascript

---
+++ js/operators.js
@@ -0,0 +1,18 @@
+//done by Ayamba Bless B
+ //javascript operators
+
+ //Equality operator
+
+let a = 32;
+let b = 12;
+if(a === b){
+    console.log("same values");
+}
+
+else{
+    console.log("different values");
+}
+
+//Unary operation
+let year = "2022";
+console.log(++year);
