4f36b44fe80c6029aba20b2842e4b7d29a0412b7
AYAM-BLESS <blessbissong@gmail.com>
2022-02-23 02:53:13 -0800

    comparing 3 numbers using operators and displaying the maximum of the 3 numbers

---
+++ js/all_operators.js
@@ -0,0 +1,15 @@
+//done by Ayamba bless B on the 23/02/2022
+/* walking with all operators. Question: write a program to find the maximum of 3 numbers*/
+let num1 = 20, num2 = 6, num3 = 13;
+if(num1 > num2 && num1 > num3){
+    console.log("maximum number is = num1");
+}
+else if (num2 > num1 && num2 > num3){
+    console.log("maximum number is = num2");
+}
+else if (num3 > num1 && num3 > num2){
+    console.log("maximum number is = num3");
+}
+else{
+    console.log("all the numbers are thesame");
+}
