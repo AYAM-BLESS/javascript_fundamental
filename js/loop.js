3f375807a49d95c96f41e2571eec220462391d12
AYAM-BLESS <blessbissong@gmail.com>
2022-02-23 03:46:04 -0800

    while loop

---
+++ js/loop.js
@@ -0,0 +1,10 @@
+//done by Ayamba bless
+//A loop is a sequence of instructions that is continually repeated untill a certain condion is met.
+//Types of loops
+/*for loop, do-while loop, while loop*/
+//while loop
+let count = 0
+while(count < 10){
+    document.write(count," learnig while loop <br />");
+    count++;
+}
