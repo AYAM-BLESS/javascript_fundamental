b3f8a6b5f19890695e620b996a0fc238f7075258
AYAM-BLESS <blessbissong@gmail.com>
2022-02-23 04:12:16 -0800

    for loop

---
+++ js/for_loop.js
@@ -0,0 +1,13 @@
+//done by ayamba bless
+//for loop
+for(let num = 1; num <= 100; num++){
+   if(num % 2 == 0){
+       document.write("even <br/>");
+   }
+   else if(num % 2 != 0){
+       document.write("odd <br/>");
+   }
+   else{
+       document.write(num, " <br/>");
+   }
+}
