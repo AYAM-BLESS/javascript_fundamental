ad8a6f4ec71d34b7ddfa58fc3d3a69c736038031
AYAM-BLESS <blessbissong@gmail.com>
2022-02-23 02:17:58 -0800

    switch statements

---
+++ js/switch_statement.js
@@ -0,0 +1,18 @@
+//done by ayamba bless
+
+//working on switch
+let city = "Buea";
+
+switch(city){
+    case "Yaounde" :
+        console.log("Yaounde is the capital of Cameroon");
+        break;
+        case "Buea" :
+            console.log("Buea is the heart of the south west region");
+            break;
+            case "Bamenda" :
+                console.log("Bamenda is the heart of the north west region");
+                break;
+                default :
+                console.log("selected city is not available");
+}
