3580962b0732f48c77af3fddffab7e2f0140ab66
AYAM-BLESS <blessbissong@gmail.com>
2022-02-23 04:53:03 -0800

    functions

---
+++ js/Functions.js
@@ -0,0 +1,12 @@
+//done by ayamba bless
+//a function is a group of reusable codes which can be called anywhere in your program
+
+function sayBless(){
+    document.write("Hi Bless");
+}
+
+function calcArea(length, width){
+    let area = length * width;
+    document.write(area);
+}
+calcArea(7, 3);
