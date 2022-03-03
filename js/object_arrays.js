26cd5ebf9e5226055ade5e9c96b126f797ff3fac
AYAM-BLESS <blessbissong@gmail.com>
2022-03-02 20:56:29 -0800

    simple object arrays

---
+++ js/object_arrays.js
@@ -0,0 +1,11 @@
+//done by ayamba bless
+// object arrays
+// Encapsulation
+// Aggregation
+// Inheritance
+// Polymorphism
+let person = new Array();
+person.name = "Bless";
+person.age = 19;
+
+document.write(person.name);
