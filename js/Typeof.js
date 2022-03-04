5f83e3392870702831b9b6b92c8f0fc7b8efedbf
AYAM-BLESS <blessbissong@gmail.com>
2022-03-03 17:01:48 -0800

    the typeof operation

---
+++ js/Typeof.js
@@ -0,0 +1,10 @@
+//done by ayamba bles
+//Typeof
+typeof(1);                    //number
+typeof(true);                 //boolean
+typeof("hello");              //string
+typeof(function(){});         //function
+typeof({});                   //object
+typeof(undefined);            //undefined
+
+console.log(typeof({}));
