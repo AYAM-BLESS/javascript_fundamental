a811b6231c2e852b0f73c5960732ad15f27855dc
AYAM-BLESS <blessbissong@gmail.com>
2022-03-03 16:31:59 -0800

    destructuring objects

---
+++ js/destructuring_objects.js
@@ -0,0 +1,11 @@
+//done by ayamba bless
+//destructuring an object
+ let car = {
+     id : 5000,
+     style : "convertible",
+ };
+//example 1
+ let {id, style} = car; //destructuring the objects
+ console.log(id, style);
+
+ 
