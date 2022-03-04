fd7af527016966a0a2cfccf940e2e33b58088693
AYAM-BLESS <blessbissong@gmail.com>
2022-03-03 17:13:38 -0800

    some common conversion in javascript

---
+++ js/conversion.js
@@ -0,0 +1,11 @@
+//done by ayamba bless 
+//convert to string
+//foo.toString();
+
+//convert string to integer
+//Number.parseInt('55');  //55 is a number
+
+//convert string to number
+//Number.parseFloat('55.99'); 55.99 is a number
+
+console.log(Number.parseFloat('55.99'));
