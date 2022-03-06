5b7252342e874d69f89661282223adc76ee69ba5
AYAM-BLESS <blessbissong@gmail.com>
2022-03-05 21:23:17 -0800

    bind.js

---
+++ js/bind.js
@@ -0,0 +1,12 @@
+//done by ayamba bless
+
+let o = {
+    carId: 123,
+    getId: function(){
+        return this.carId;
+    }
+};
+
+let newCar = { carId: 456 };
+let newFn = o.getId.bind(newCar);
+console.log(newFn());
