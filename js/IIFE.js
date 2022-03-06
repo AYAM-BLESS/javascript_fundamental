5b8251e00e0824dd170c20116c698cfcb5f9e59e
AYAM-BLESS <blessbissong@gmail.com>
2022-03-05 21:10:52 -0800

    Immediately Invoked Function Execution first example

---
+++ js/IIFE.js
@@ -0,0 +1,9 @@
+//done by ayamba bless 
+//IIFE which stands for immediately invoked function execution
+// first example
+
+(function(){
+    console.log('in function');
+})();
+
+//output is 'in function'
