6068a1fdc4915510c44578aa09d74e8b14ff4b7f
AYAM-BLESS <blessbissong@gmail.com>
2022-03-12 22:02:53 -0800

    counting the number of characters and then displays the number og characters that is remaining

---
+++ js/tweet-composer.js
@@ -0,0 +1,6 @@
+//done by ayamba bless
+// Tweet composer
+let tweet = prompt("compose your tweet");
+let tweetLength = tweet.length;
+
+alert("You have written " + tweetLength + " characters, you have " + (140-tweetLength) + " characters remaining ");
