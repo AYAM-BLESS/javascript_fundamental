f21d1a088ff6f18c5d78806e952f30159867271c
AYAM-BLESS <blessbissong@gmail.com>
2022-03-22 11:50:20 -0700

    projec 1

---
+++ js/project1.js
@@ -0,0 +1,13 @@
+//done by Ayamba Bless
+/* In this challenge you are going to create a function which tells us how many days, weeks, and month we have left
+if we live till 90 years.
+Assuming that 365 days in a year, 52 weeks in a year and 12 months in a year.*/
+// Output should be you have X days, Y weeks and Z months left.
+//solution to this problem
+
+let age = prompt("Enter your age");
+let remainingYears = 90 - age;
+let days = remainingYears *365;
+let weeks = remainingYears *52;
+let months = remainingYears *12;
+alert(" You have " + days + " days, " + weeks  + " weeks and " + months + " months left. ");
