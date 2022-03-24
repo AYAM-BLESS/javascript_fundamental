8ff4c21c23f4d676563f499df8b79d8d5106bc15
AYAM-BLESS <blessbissong@gmail.com>
2022-03-23 22:19:58 -0700

    Love calculator

---
+++ js/loveCalculator.js
@@ -0,0 +1,7 @@
+//done by Ayamba Bless
+//Love calculator
+let myName = prompt("What is your name");
+let herName = prompt("What is her name");
+let loveScore = Math.random() * 100;
+loveScore = Math.floor(loveScore);
+alert(`The love score for ${myName} and ${herName} is ${loveScore}%`)
