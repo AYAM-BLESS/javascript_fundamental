ce3cbd067bf7faf2e18c163b44fd1a68a89d25a2
AYAM-BLESS <blessbissong@gmail.com>
2022-03-30 21:34:49 -0700

    minor changes

--- js/numbers.js
+++ js/numbers.js
@@ -1,11 +1,11 @@
 //done by ayamba bless
-let num1 = 3;
+let num1 = 4;
 let num2 = 5;
 document.write(num1 + num2 + "<br>");
-let num3 = 4;
-let num4 = "6";
+let num3 = 7;
+let num4 = "9";
 document.write(num3 + num4 +"<br>");
-let num5 = 6.5;
+let num5 = 8.5;
 let num6 = Math.round(num5);
 document.write(num6 +"<br><br>");
 //generating random values
